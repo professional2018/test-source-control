009d18a5de3e96cf429b7903dd4b2cf54bce2643
etudiant <etudiant@etudiant-6.local>
2018-08-22 10:24:47 +0200

    Second commit

--- Test  du Source Control/ViewController.swift
+++ Test  du Source Control/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("1er commit")
+        print("2eme commit")
         
     }
 
