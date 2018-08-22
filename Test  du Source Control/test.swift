775d93535afc21a4c574d18e08df55c8b782ef30
etudiant <etudiant@etudiant-6.local>
2018-08-22 11:25:51 +0200

    ajout class test

---
+++ Test  du Source Control/test.swift
@@ -0,0 +1,17 @@
+//
+//  test.swift
+//  Test  du Source Control
+//
+//  Created by etudiant on 22/08/2018.
+//  Copyright © 2018 etudiant. All rights reserved.
+//
+
+import Foundation
+
+
+class test{
+    
+    func salut (){
+        print("salut les codabeilles")
+    }
+}
