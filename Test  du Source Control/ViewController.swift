ff399206dd4298a7e0552390226cb99f69dfc56b
etudiant <etudiant@etudiant-6.local>
2018-08-22 10:42:05 +0200

    Revert 2nd commit

--- Test  du Source Control/ViewController.swift
+++ Test  du Source Control/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("2eme commit")
+        print("1er commit")
         
     }
 
